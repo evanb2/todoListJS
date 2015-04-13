a8cb8aa7c8e2d17cc1a9f7aac64ef272ea8538f4
geoff winner & abeer khakwani <student@epicodus.com>
2015-04-13 15:52:02 -0700

    doneish

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,35 @@ $(document).ready(function() {
 
         var taskInput = $("input#task").val();
         var taskObject= {task:taskInput};
-        //var CompletedTask={};
+        var CompletedTask={};
 
-        $("#tasks").append("<span class='task'>" + taskObject.task + "<form id='completed'><input type='radio' name='task' value='completed'>Completed</form>"+ "</span></br>");
+        $("#tasks").append("<span class='task'>" + taskObject.task + "</span></br>");
 
+
+        $(".task").last().click(function(){
+            $(this).remove();
+            $(".complete").append(taskObject.task + "</br>");
+            // $(".hate").click(function(){
+            //     $(this).remove();
+            //      $("#tasks").append("<span class='task'>" + taskObject.task + "</span></br>");
+            //
+            //             //  $(".task").last().click(function(){
+            //             //      $(this).remove();
+            //             //      $("#completed").append("<div class='hate'>" + taskObject.task + "</div></br>");
+            //             //      $(".hate").click(function(){
+            //             //          $(this).remove();
+            //             //           $("#tasks").append("<span class='task'>" + taskObject.task + "</span></br>");
+            //             //      });
+            //             //  });
+            // });
+        });
 
 
     });
+
+
+
+
+
+
 });
