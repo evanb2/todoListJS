5245b21587fb10fac26f77f3e6e36db07ca91f60
Evan Butler & Abeer Khakwani <student@epicodus.com>
2015-04-16 10:49:25 -0700

    add each loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,39 @@
 $(document).ready(function() {
-    $("input#task").focus();
-    $('form#to_do').submit(function(event) {
-        event.preventDefault()
+    $("input#name").focus();
+    // $('form#to_do').submit(function(event) {
+    //     event.preventDefault()
+    //
+    //     var taskInput = $("input#task").val();
+    //     var taskObject= {task:taskInput};
+    //     var CompletedTask={};
+    //
+    //     $("#tasks").append("<li>" + taskObject.task + "</li>");
+    //
+    //     $("input#task").val("");
+    //
+    //
+    //     $("li").last().click(function(){
+    //         $(this).remove();
+    //         $("#completed").append("<li>" + taskObject.task + "</li>");
+    //     });
+    // });
 
-        var taskInput = $("input#task").val();
-        var taskObject= {task:taskInput};
-        var CompletedTask={};
+    $("form#create_list").submit(function(event) {
+        event.preventDefault();
 
-        $("#tasks").append("<li>" + taskObject.task + "</li>");
+        var listName = $("input#name").val();
 
-        $("input#task").val("");
+        var newlistObject={ list: listName, tasks: [] };
 
-
-        $("li").last().click(function(){
-            $(this).remove();
-            $("#completed").append("<li>" + taskObject.task + "</li>");
-            // $(".hate").click(function(){
-            //     $(this).remove();
-            //      $("#tasks").append("<span class='task'>" + taskObject.task + "</span></br>");
-            //
-            //             //  $(".task").last().click(function(){
-            //             //      $(this).remove();
-            //             //      $("#completed").append("<div class='hate'>" + taskObject.task + "</div></br>");
-            //             //      $(".hate").click(function(){
-            //             //          $(this).remove();
-            //             //           $("#tasks").append("<span class='task'>" + taskObject.task + "</span></br>");
-            //             //      });
-            //             //  });
-            // });
+        $("#newTasks").each(function() {
+            var taskName = $(this).find("input#task").val();
+            var newTask = { task: taskName };
+            newlistObject.tasks.push(newTask);
         });
+        console.log(newlistObject.tasks);
 
+        $("#lists ul").append("<li>" + newlistObject.list + newlistObject.tasks + "</li>");
 
+        $("input#name").val("");
     });
-
-
-
-
-
-
 });
